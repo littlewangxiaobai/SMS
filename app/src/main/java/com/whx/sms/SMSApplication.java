08fa2b37916c4dae4a229fe6c39df83e5cc1eabf
whx <love_today@126.com>
2019-07-11 16:35:42 +0800

    bugly

--- app/src/main/java/com/whx/sms/SMSApplication.java
+++ app/src/main/java/com/whx/sms/SMSApplication.java
@@ -1,15 +1,14 @@
 package com.whx.sms;
 
 import android.app.Application;
-import android.content.Intent;
-import android.provider.Telephony;
+
+import com.tencent.bugly.crashreport.CrashReport;
 
 public class SMSApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-
+        CrashReport.initCrashReport(getApplicationContext(), "7ab4b184e2", true);
     }
 }
